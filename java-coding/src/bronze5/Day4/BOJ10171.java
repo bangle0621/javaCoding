11b835f0800046141f69bb87ca3816178da3df98
lmh0621 <lmh0621@ectech.co.kr>
2024-05-30 23:53:28 +0900

    Day4

---
+++ java-coding/src/bronze5/Day4/BOJ10171.java
@@ -0,0 +1,18 @@
+package bronze5.Day4;
+
+public class BOJ10171 {
+	public static void main(String[] args) {
+// 일반적으로 출력이 안되는 문자열이 혼재
+// " 출력 위해서는 \" 로 사용, \출력 위해서는 \\
+//		\    /\
+//		 )  ( ')
+//		(  /  )
+//		 \(__)|
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		System.out.println("\"\"");
+		
+	}
+}
