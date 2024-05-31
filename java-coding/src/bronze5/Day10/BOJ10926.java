b1f2b17762928dcdc38e8643741044bd5adcec97
lmh0621 <lmh0621@ectech.co.kr>
2024-05-31 23:32:35 +0900

    Day10

---
+++ java-coding/src/bronze5/Day10/BOJ10926.java
@@ -0,0 +1,17 @@
+package bronze5.Day10;
+
+import java.util.Scanner;
+
+public class BOJ10926 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String orgId = sc.next();
+		
+		System.out.println(orgId+"??!");
+		
+	}
+
+}
---
+++ java-coding/src/bronze5/Day10/BOJ11654.java
@@ -0,0 +1,23 @@
+package bronze5.Day10;
+
+import java.util.Scanner;
+
+public class BOJ11654 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//아스키코드 값 얻기 : 문자 -> 캐릭터 형 -> int
+		Scanner sc = new Scanner(System.in);
+		
+//		String A = sc.next();
+//		
+//		char text = A.charAt(0);
+//		System.out.println((int)text);
+		
+		int ascii = sc.nextInt();
+		
+		System.out.println((char)ascii);
+		
+	}
+
+}
