e4cff382e66060e2525a80678e7253ac85968ebd
lmh0621 <lmh0621@ectech.co.kr>
2024-05-31 22:11:15 +0900

    Day6

---
+++ java-coding/src/bronze5/Day6/BOJ2741.java
@@ -0,0 +1,21 @@
+package bronze5.Day6;
+
+import java.util.Scanner;
+
+public class BOJ2741 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for(int i=1;i<N+1
+				;i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
